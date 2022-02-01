3c9344f8c38323950f9996abfeda5fc39cbfe83c
Wojtek <wojtek.bogdziun@gmail.com>
2022-02-01 19:43:58 +0100

    Add project files.

---
+++ T4Z2/FizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4Z2
+{
+    class FizzBuzz
+    {
+        public  string CheckFizzBuzz(int number)
+        {
+            string fizzBuzz = String.Empty;
+            if (number % 3 == 0)
+                fizzBuzz += "Fizz";
+            if (number % 5 == 0)
+                fizzBuzz += "Buzz";
+            if (fizzBuzz.Equals(String.Empty))
+                fizzBuzz = number.ToString();
+            return fizzBuzz;
+        }
+    }
+}
---
+++ T4Z2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace T4Z2
+{
+    class Program
+    {
+        private static FizzBuzz fizzBuzz = new FizzBuzz();
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("Podaj liczbę: ");
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    string result = fizzBuzz.CheckFizzBuzz(number);
+                    Console.WriteLine($"Wynik: {result}");
+                }
+                else
+                    Console.WriteLine("Podano nieprawidłową liczbę");
+
+                Console.WriteLine("Aby kontynuować naciśnij dowolny klawisz, aby wyjść naciśnij w");
+                if (Char.ToUpper(Console.ReadKey().KeyChar) == 'W')
+                    return;
+            }
+        }
+    }
+}
